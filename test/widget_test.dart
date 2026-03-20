cbf5fd0f647862d363a4be2db9c4dae607df98ed
fortresswilson <108816788+fortresswilson@users.noreply.github.com>
2026-03-20 19:42:42 -0400

    Adds the creates user data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:campus_challenge_rewards_app_mad/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CampusQuestApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
